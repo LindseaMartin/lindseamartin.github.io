f23c5272739f0f7f129e096d2ef4d2c85fa1b443
LindseaMartin <ninzerb@gmail.com>
2023-01-22 02:55:18 -0700

    Revert "Merge remote-tracking branch 'origin/gh-pages' into gh-pages"
    This reverts commit 9dfa5004e4609d5584717aa23725ca12c19ab9bd, reversing
    changes made to 71f45eb004c07a9a6c576f77185f446e51d16248.

---
+++ src/data/SkillsData.js
@@ -0,0 +1,92 @@
+import {
+	FaBootstrap,
+	FaCss3Alt,
+	FaGithub,
+	FaHtml5,
+	FaNpm,
+	FaReact,
+	FaSass,
+	FaFigma
+} from "react-icons/fa";
+import { DiJavascript1 } from "react-icons/di";
+import { VscJson } from "react-icons/vsc";
+import {
+	SiAdobephotoshop,
+	SiTailwindcss,
+	SiNextdotjs,
+	SiStyledcomponents,
+	SiFramer,
+	SiMicrosoftsqlserver,
+	SiCsharp
+} from "react-icons/si";
+
+export const SkillsData = [
+	{
+		name: "HTML",
+		icon: <FaHtml5 />,
+	},
+	{
+		name: "CSS",
+		icon: <FaCss3Alt />,
+	},
+	{
+		name: "Javascript",
+		icon: <DiJavascript1 />,
+	},
+	{
+		name: "React Js",
+		icon: <FaReact />,
+	},
+	{
+		name: "Next Js",
+		icon: <SiNextdotjs />,
+	},
+	{
+		name: "C#",
+		icon: <SiCsharp />,
+	},
+	{
+		name: "JSON",
+		icon: <VscJson />,
+	},
+	{
+		name: "Bootstrap",
+		icon: <FaBootstrap />,
+	},
+	{
+		name: "Tailwind",
+		icon: <SiTailwindcss />,
+	},
+	{
+		name: "Sass",
+		icon: <FaSass />,
+	},
+	{
+		name: "Styled Components",
+		icon: <SiStyledcomponents />,
+	},
+	{
+		name: "Framer Motion",
+		icon: <SiFramer />,
+	},
+	{
+		name: "Git",
+		icon: <FaGithub />,
+	},
+	{
+		name: "Npm",
+		icon: <FaNpm />,
+	},
+	{
+		name: "Figma",
+		icon: <FaFigma />,
+	},
+	{
+		name: "Photoshop",
+		icon: <SiAdobephotoshop />,
+	},
+	{
+		name: "SQL",
+		icon: <SiMicrosoftsqlserver />,
+	}
+];
